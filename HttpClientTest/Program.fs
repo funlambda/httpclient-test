01bc505cda3d88cd3498d3b5681fbe5ae8121114
Brooks Noble <brooks.noble@gmail.com>
2017-04-26 21:36:44 -0400

    Initial version: just get and print google homepage.

---
+++ HttpClientTest/Program.fs
@@ -0,0 +1,9 @@
+﻿open System.Net.Http
+
+[<EntryPoint>]
+let main argv = 
+    let client = new HttpClient()
+    let response = client.GetAsync("http://www.google.com/") |> Async.AwaitTask |> Async.RunSynchronously
+    let str = response.Content.ReadAsStringAsync() |> Async.AwaitTask |> Async.RunSynchronously
+    printfn "%A" str
+    0 // return an integer exit code
